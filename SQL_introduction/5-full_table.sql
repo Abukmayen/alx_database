d62844f44fec97132bc0b5d4290201411a153115
Abukmayen <abukmayen123@gmail.com>
2023-08-13 00:57:54 -0700

    5-full_table.sql

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,11 @@
--- Prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server
-SHOW CREATE TABLE first_table;
+-- Create database and table
+DROP DATABASE IF EXISTS hbtn_test_db_5;
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_5;
+USE hbtn_test_db_5;
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(128),
+    c CHAR(1),
+    created_at DATE,
+    PRIMARY KEY (id)
+);
